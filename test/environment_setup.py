ae8794914b55f41e1be62bad8a59e6185350624f
Cole Foster <cmf290@miami.edu>
2025-03-31 18:04:39 -0400

    Environment variable config reader

---
+++ test/environment_setup.py
@@ -0,0 +1,76 @@
+import yaml
+import sys
+import os
+
+# Validate the config file
+if len(sys.argv) != 2 or not isinstance(sys.argv[1], str) \
+    or not os.path.exists(sys.argv[1]) \
+    or os.path.splitext(sys.argv[1])[1] not in [".yml", ".yaml"]:
+    print("Usage: metis <path>")
+    print("Please provide a valid path to the configuration file (YAML)")
+    exit(1)
+
+# Read the configuration file
+config_path = sys.argv[1]
+with open(config_path, "r") as f:
+        config = yaml.safe_load(f)
+
+# Validate a config and field, store in errors list
+def validate_field(config: dict, field: str, field_type: type, errors: list) -> None:
+    if field not in config:
+        errors.append(f"Error: '{field}' field not found in configuration")
+    elif not isinstance(config[field], field_type):
+        errors.append(f"Error: Invalid {config} configuration, requires a {field_type} for '{field}'")
+
+# Store config errors
+errors = []
+
+# Validate the database configuration
+if "Database" not in config:
+    errors.append("Error: 'Database' field not found in the configuration file")
+    
+else:
+    
+    # Validate fields in the database configuration
+    database = config["Database"]
+    for field in ["Host", "User", "Password", "Name"]:
+        validate_field(database, field, str, errors)
+    validate_field(database, "Port", int, errors)
+    
+# Validate the scraper configuration
+if "Scraper" not in config:
+    errors.append("Error: 'Scraper' field not found in the configuration file")
+    
+else:
+    
+    # Validate fields in the scraper configuration
+    scraper = config["Scraper"]
+    validate_field(scraper, "Host", str, errors)
+    validate_field(scraper, "Endpoints", list, errors)
+    for field in ["Interval", "Port"]:
+        validate_field(scraper, field, int, errors)
+    
+    # Validate individual endpoints
+    if "Endpoints" in scraper:
+        for endpoint in scraper["Endpoints"]:
+            if not isinstance(endpoint, str):
+                errors.append("Error: Invalid endpoint configuration, requires a list of strings")
+                break
+
+# Exit if errors are found
+if errors:
+    for error in errors:
+        print(error)
+    exit(1)
+
+# If no errors are found, write the configuration to a .env file
+with open(".env", "w") as f:
+    f.write(f"DB_HOST={database['Host']}\n")
+    f.write(f"DB_PORT={database['Port']}\n")
+    f.write(f"DB_USER={database['User']}\n")
+    f.write(f"DB_PASSWORD={database['Password']}\n")
+    f.write(f"DB_NAME={database['Name']}\n")
+    f.write(f"SCRAPER_INTERVAL={scraper['Interval']}\n")
+    f.write(f"SCRAPER_ENDPOINTS={','.join(scraper['Endpoints'])}\n")
+    f.write(f"SCRAPER_PORT={scraper['Port']}\n")
+    f.write(f"SCRAPER_HOST={scraper['Host']}\n")
