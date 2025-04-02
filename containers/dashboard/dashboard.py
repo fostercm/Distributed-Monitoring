5bd03ce4a21bd50d1486f209d02b5f58ec0b9f93
Cole Foster <cmf290@miami.edu>
2025-04-02 00:53:12 -0400

    Simplified imports

--- containers/dashboard/dashboard.py
+++ containers/dashboard/dashboard.py
@@ -2,8 +2,6 @@ from redis import Redis
 import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import time
 
 # Connect to Redis
